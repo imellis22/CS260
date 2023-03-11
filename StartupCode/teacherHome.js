c2efb948d580f42a4134e05b70dac5befa35068b
imellis22 <isaac.22.ellis@gmail.com>
2023-03-10 23:46:44 -0700

    done

--- StartupCode/teacherHome.js
+++ StartupCode/teacherHome.js
@@ -1,4 +1,15 @@
+let numStudents = 0;
+let students = []; 
+const theStudent = 
+{
+    number:"0",
+    question:""
+}
+
 function addStudent() {
+    const theStudent = new Object();
+    theStudent.number = numStudents;
+
     const studentList = document.querySelector('#main-page');
 
     const student = document.createElement('div'); //the main div for the student
@@ -11,6 +22,11 @@ function addStudent() {
     const question = document.createElement('div');
     question.classList.add('question');
     question.textContent = "Question will go here";
+    theStudent.question = question.textContent;
+
+    console.log(theStudent.number);
+    console.log(theStudent.question);
+    students.push(theStudent);
 
     name.appendChild(question); //adds the question child to the name div
 
@@ -22,7 +38,10 @@ function addStudent() {
     const responseButton = document.createElement('button');
 
     responseButton.classList.add('btn', 'btn-primary', 'response-btn');
+    responseButton.type = "submit";
+    responseButton.id = numStudents;
     responseButton.textContent = "Give Answer";
+    responseButton.onclick = makeModal;
 
     buttons.appendChild(responseButton);
 
@@ -30,6 +49,7 @@ function addStudent() {
     
 
     studentList.appendChild(student);
+    ++numStudents;
 }
 
 function setName(){
@@ -37,9 +57,13 @@ function setName(){
     userName.textContent = "Welcome " + localStorage.getItem('teacherName');
 }
 
-function makeModal(){
+function makeModal(clicked_id){
+    const num = clicked_id;
+    console.log(num);
     const modal = document.getElementById("theModal");
     modal.style.display = "block";
+    const question = students[0].question;
+    console.log(question);
 }
 
 function closeModal(){
